e0decb18397ae6c29be6e56bc800ab730cf74394
Uvarov-is <ilya.uvarov@magenta-technology.com>
2017-01-31 19:02:53 +0400

    Ready_First_Todos

--- MainScreen.java
+++ MainScreen.java
@@ -1,13 +1,21 @@
 package com.example.uvarov_is.kyza;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.widget.Toast;
+
+import static com.example.uvarov_is.kyza.R.string.TextToast;
 
 public class MainScreen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        Toast toastHello = Toast.makeText(getApplicationContext(), getString(TextToast), Toast.LENGTH_LONG);
+        toastHello.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
+        toastHello.show();
     }
 }
